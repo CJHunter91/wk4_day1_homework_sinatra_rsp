ff468e1dee4fc4f9c890f324ad40b10104cccba1
Chris Hunter <orkem2@gmail.com>
2017-07-17 16:04:12 +0100

    Added route in controller which creates a new game object and returns what won.

--- controller.rb
+++ controller.rb
@@ -1,6 +1,12 @@
 require('sinatra')
-require('sinatra/contrib')
+require('sinatra/contrib/all')
+require_relative('models/rps')
 
 get "/" do
   "Homepage"
+end
+
+get "/game/:player1/:player2" do
+  new_game = RockPaperScissors.new(params[:player1], params[:player2])
+  return new_game.play
 end
