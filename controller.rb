411b9ab7c4352e3a45870ab13943f4f5281d020a
Chris Hunter <orkem2@gmail.com>
2017-07-17 16:32:25 +0100

    Added welcome page and created game and homepage erb view files.

--- controller.rb
+++ controller.rb
@@ -3,10 +3,12 @@ require('sinatra/contrib/all')
 require_relative('models/rps')
 
 get "/" do
-  "Homepage"
+  erb(:homepage)
 end
 
+
+
 get "/game/:player1/:player2" do
-  new_game = RockPaperScissors.new(params[:player1], params[:player2])
-  return new_game.play
+  erb(:game)
 end
+
